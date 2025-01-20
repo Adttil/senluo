5fa64e3bb3f2d70d00a4d50624d63a3ed85eedc8
Adttil <1963096826@qq.com>
2025-01-21 03:09:20 +0800

    add concepts for tag tables

--- include/senluo/core_detail/principle.hpp
+++ include/senluo/core_detail/principle.hpp
@@ -20,15 +20,56 @@ namespace senluo
 
         template<auto OperationTree>
         struct operate_t;
+
+        template<auto Constexpr>
+        consteval auto pass_constexpr()
+        {
+            return Constexpr;
+        }
+
+        template<class T>
+        constexpr bool is_layout()
+        {
+            if constexpr(indexical<T>)
+            {
+                return true;
+            }
+            else return []<size_t...I>(std::index_sequence<I...>)
+            {
+                return (... && detail::is_layout<subtree_t<T&, I>>());
+            }(std::make_index_sequence<size<T>>{});
+        }
+
+        template<class T, class TTag>
+        constexpr bool is_tag_tree()
+        {
+            if constexpr(std::same_as<std::remove_cvref_t<T>, TTag>)
+            {
+                return true;
+            }
+            else return []<size_t...I>(std::index_sequence<I...>)
+            {
+                return (... && detail::is_tag_tree<subtree_t<T&, I>, TTag>());
+            }(std::make_index_sequence<size<T>>{});
+        }
     }
 
-    template<auto Layout>
+    template<class T>
+    concept indexes_tree_liked = detail::is_layout<T>();
+
+    template<class T>
+    concept stricture_tree_liked = detail::is_tag_tree<T, stricture_t>();
+
+    template<class T>
+    concept operation_tree_liked =  detail::is_tag_tree<T, operation_t>();
+
+    template<indexes_tree_liked auto Layout>
     inline constexpr detail::relayout_t<Layout> relayout{};
 
-    template<auto StrictureTree>
+    template<stricture_tree_liked auto StrictureTree>
     inline constexpr detail::astrict_t<detail::fold_tag_tree<StrictureTree>()> astrict{};
 
-    template<auto OperationTree>
+    template<operation_tree_liked auto OperationTree>
     inline constexpr detail::operate_t<detail::fold_operation_tree<OperationTree>()> operate{};
 }
 
@@ -46,9 +87,9 @@ namespace senluo
     {
         { []<class U>(principle_interface<U>&)->U*{}(t) } -> std::same_as<decltype(t)*>;
         data(std::declval<T>());
-        decltype(t)::layout();
-        decltype(t)::stricture_tree();
-        decltype(t)::operation_tree();
+        { detail::pass_constexpr<decltype(t)::layout()>() } -> indexes_tree_liked;
+        { detail::pass_constexpr<decltype(t)::stricture_tree()>() } -> stricture_tree_liked;
+        { detail::pass_constexpr<decltype(t)::operation_tree()>() } -> operation_tree_liked;
     };
     
     namespace detail::principle_t_ns 
@@ -75,14 +116,17 @@ namespace senluo
         }
     }
     
-    template<auto UsageTree>
+    template<class T>
+    concept usage_tree_liked =  detail::is_tag_tree<T, usage_t>();
+
+    template<usage_tree_liked auto UsageTree>
     inline constexpr detail::principle_t_ns::principle_fn<UsageTree> principle;
     
-    template<typename T, auto UsageTree>
+    template<typename T, usage_tree_liked auto UsageTree>
     using principle_t = decltype(std::declval<T>() | principle<UsageTree>);
 
     template<class T, auto UsageTree>
-    concept plain = (detail::principle_t_ns::is_plain<T, UsageTree>());
+    concept plain = usage_tree_liked<decltype(UsageTree)> && (detail::principle_t_ns::is_plain<T, UsageTree>());
 
     template<class Pretreater>
     struct pretreater_interface : adaptor_closure<Pretreater>
@@ -171,13 +215,13 @@ namespace senluo
         };
     }
 
-    template<auto UsageTree>
+    template<usage_tree_liked auto UsageTree>
     inline constexpr detail::sequence_by_usage_t<UsageTree> sequence_by_usage{};
 
-    template<auto UsageTree>
+    template<usage_tree_liked auto UsageTree>
     inline constexpr detail::inverse_sequence_by_usage_t<UsageTree> inverse_sequence_by_usage{};
 
-    template<auto UsageTree>
+    template<usage_tree_liked auto UsageTree>
     inline constexpr detail::seperate_by_usage_t<UsageTree> seperate_by_usage{};
 
     template<class Tree>
@@ -318,7 +362,7 @@ namespace senluo
         };
     }
     
-    template<auto UsageTree = usage_t::once, template<class...> class Tpl = tuple>
+    template<usage_tree_liked auto UsageTree = usage_t::once, template<class...> class Tpl = tuple>
     inline constexpr detail::plainize_fn<UsageTree, Tpl> plainize{};
     
     template<template<class...> class Tpl = tuple>
@@ -345,7 +389,7 @@ namespace senluo
         };
     };
 
-    template<auto UsageTree = usage_t::once, template<class...> class Tpl = tuple>
+    template<usage_tree_liked auto UsageTree = usage_t::once, template<class...> class Tpl = tuple>
     inline constexpr detail::plainize_principle_fn<UsageTree, Tpl> plainize_principle{};
 }
 
