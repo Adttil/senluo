e21e07e0fa67187ec313582c3eef99297ed39678
adttilove <1963096826@qq.com>
2026-03-29 17:06:36 +0800

    use explicit make

--- compile_time_benchmark/tuple_zip_transform.cpp
+++ compile_time_benchmark/tuple_zip_transform.cpp
@@ -3,7 +3,9 @@
 int main()
 {
     auto tpl = senluo::tuple{1, 2, 3};
-    tpl = +senluo::zip_transform(std::plus<>{}, tpl, tpl);
+    using tuple_t = decltype(tpl);
+    tpl = senluo::zip_transform(std::plus<>{}, tpl, tpl) | senluo::make<tuple_t>;
+
 
     return 0;
 }
